b37a89d9cc76b6cb52493d3091d648f6b6f04156
Sam Parker <48276760+sparkerBCI@users.noreply.github.com>
2020-06-17 15:57:13 +1000

    Calculated onboard correct rate is 77.05

---
+++ Software/SerialTest/checker.m
@@ -0,0 +1,16 @@
+score = 0;
+for i = 1:122
+    if (i > 1)
+        classi = cell2mat(output_test(i));
+        classi = classi(2:end)
+    else
+        classi = output_test{i}
+    end
+   y = cell2mat(YTest(i))
+    if (strcmp(classi, y))
+        score = score + 1;
+    end
+    
+end
+
+CorrectRate = score / 122
