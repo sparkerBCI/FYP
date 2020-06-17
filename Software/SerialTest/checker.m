9a8d7a48fdeade645d874567deaaa9c754df9be7
Sam Parker <48276760+sparkerBCI@users.noreply.github.com>
2020-06-17 16:00:34 +1000

    Added screenshot, removed prints to console

--- Software/SerialTest/checker.m
+++ Software/SerialTest/checker.m
@@ -2,15 +2,15 @@ score = 0;
 for i = 1:122
     if (i > 1)
         classi = cell2mat(output_test(i));
-        classi = classi(2:end)
+        classi = classi(2:end);
     else
-        classi = output_test{i}
+        classi = output_test{i};
     end
-   y = cell2mat(YTest(i))
+   y = cell2mat(YTest(i));
     if (strcmp(classi, y))
         score = score + 1;
     end
     
 end
 
-CorrectRate = score / 122
+CorrectRate = score / 122 * 100
